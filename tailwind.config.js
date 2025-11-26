61786ce149b07142076f5962d5c06bd94dee6881
taskpul <romtietz@gmail.com>
2025-11-26 17:11:08 +0100

    Fix build paths and Tailwind content globs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-
+        "./resources/**/*.{js,jsx,ts,tsx,vue,scss,css}",
+        "./app/**/*.{php,vue}",
+        "./language/**/*.php",
     ],
     theme: {
         extend: {},
