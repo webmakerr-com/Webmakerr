0c41be56d1efb37256505dde1287d17aee93874d
taskpul <romtietz@gmail.com>
2025-11-28 05:10:52 +0100

    Show add to cart for subscription products

--- resources/public/single-product/SingleProduct.js
+++ resources/public/single-product/SingleProduct.js
@@ -503,14 +503,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
                 this.#setupBuyNowButton(variationId, status);
 
-                if (button.dataset.paymentType === 'subscription') {
-                    this.#addToCartButtons.forEach(button => button.classList.add('is-hidden'));
-                } else {
-                    this.#addToCartButtons.forEach(button => {
-                        button.classList.remove('is-hidden');
-                        button.setAttribute('data-cart-id', variationId);
-                    });
-                }
+                this.#addToCartButtons.forEach(button => {
+                    button.classList.remove('is-hidden');
+                    button.setAttribute('data-cart-id', variationId);
+                });
             }
 
 
@@ -762,12 +758,9 @@ document.addEventListener('DOMContentLoaded', () => {
         }
 
         #initiallyHideAddToCartButton() {
-            let paymentType = this.#variationButtons[0]?.dataset.paymentType;
-            if (paymentType === 'subscription') {
-                this.#addToCartButtons.forEach(button => {
-                    button.setAttribute('data-cart-id', '');
-                });
-            }
+            this.#addToCartButtons.forEach(button => {
+                button.classList.remove('is-hidden');
+            });
         }
 
         #initiallyHideOutOfStockButton() {
