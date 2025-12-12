b7649bb7840f1516a50151da8e9484715b99e9d6
taskpul <romtietz@gmail.com>
2025-12-12 13:13:03 +0100

    Ensure legacy loader stays hidden

--- fluent-cart.php
+++ fluent-cart.php
@@ -2,12 +2,14 @@
 
 defined('ABSPATH') or die;
 
-if (defined('WEBMAKERR_PLUGIN_FILE')) {
+if (defined('WEBMAKERR_LEGACY_LOADER')) {
     return;
 }
 
-if (!defined('WEBMAKERR_LEGACY_LOADER')) {
-    define('WEBMAKERR_LEGACY_LOADER', __FILE__);
+define('WEBMAKERR_LEGACY_LOADER', __FILE__);
+
+if (defined('WEBMAKERR_PLUGIN_FILE')) {
+    return;
 }
 
 require __DIR__ . '/webmakerr.php';
